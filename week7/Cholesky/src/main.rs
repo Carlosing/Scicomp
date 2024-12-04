9c1f257734718834f7b90bf2da62cecf143fa132
Carlosing <96519158+Carlosing@users.noreply.github.com>
2024-12-04 17:58:32 +0000

    week7 Function converts file into CSR vector

--- week7/Cholesky/src/main.rs
+++ week7/Cholesky/src/main.rs
@@ -14,35 +14,42 @@ fn main() {
 
     let lines: Vec<String> = reader.collect::<Result<_, _>>().expect("Error reading lines");
 
-    let line1 = &lines[0];
-
-    let sizes: Vec<usize> = line1.split_whitespace().map(|x| x.parse().unwrap()).collect(); 
-
-    let (rows, columns, nnz) = (sizes[0], sizes[1], sizes[2]);
-
-    let matriz1 = Matrix::new(rows, columns, nnz);
-
-    let matriz2 = Matrix::new(2,3,4);
-
     
 
+    let zero_vector = vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+    let one_vector = vec![0, 1, 1, 2, 0, 1, 2, 3, 1, 2, 3, 2];
+
+    let two_vector = vec![0, 2, 4, 6, 8];
+
+    let matriz1 = Matrix {
+        rows: 4,
+        columns: 4,
+        nnz: 11,
+        values: zero_vector,
+        col_indices: one_vector,
+        row_ptr: two_vector,
+    };
+
+    let matriz2 = convert_file_CSR(lines);
+    
+    
 
 
     
 }
 
 
-struct Matrix {
+struct Matrix<T> {
     rows : usize,
     columns : usize,
     nnz : usize,
+    values : Vec<T>,
+    col_indices : Vec<usize>,
+    row_ptr : Vec<usize>,
 }
 
-impl Matrix{
-    fn new(rows : usize, columns : usize, nnz: usize) -> Self {
-        Matrix {rows, columns, nnz}
-    }
-}
+
 
 
 
@@ -65,3 +72,82 @@ pub fn open_file(path: &Path) -> io::Result<File> {
     // Function to open and read the contents of a file
     File::open(path) // Opening the file at the specified path
 }
+
+
+
+// fn convert_file_CSR(lines: Vec<String>) -> (Vec<i32>, Vec<usize>, Vec<usize>) {
+//     let mut values = Vec::new();
+//     let mut col_indices = Vec::new();
+//     let mut row_ptr = Vec::new();
+//     let mut current_row = 0;
+
+//     row_ptr.push(0);
+
+//     for line in lines.iter().skip(1) {
+//         let entries: Vec<&str> = line.split_whitespace().collect();
+//         for entry in entries {
+//             let parts: Vec<&str> = entry.split(':').collect();
+//             let col_index: usize = parts[0].parse().unwrap();
+//             let value: i32 = parts[1].parse().unwrap();
+//             values.push(value);
+//             col_indices.push(col_index);
+//         }
+//         current_row += 1;
+//         row_ptr.push(values.len());
+//     }
+
+//     (values, col_indices, row_ptr)
+// }
+
+
+
+fn convert_file_CSR(lines: Vec<String>) -> (Vec<i32>, Vec<usize>, Vec<usize>, usize, usize, usize) {
+
+    let mut values = Vec::new();
+    let mut col_indices = Vec::new();
+    
+    let mut current_row = 0;
+    
+
+    
+
+    let line1 = &lines[0];
+
+    let sizes: Vec<usize> = line1.split_whitespace().map(|x| x.parse().unwrap()).collect(); 
+
+    let rows = sizes[0];
+
+    let mut row_ptr = vec![0; rows+1];
+
+    let mut row_count = vec![0; rows];
+
+    for line in lines.iter().skip(1) {
+
+        let entries: Vec<&str> = line.split_whitespace().collect();
+
+        values.push((entries[2].parse().expect("Error casting")));
+
+        let mut row: usize = entries[0].parse().expect("Error casting");
+
+        col_indices.push(entries[1].parse().expect("Error casting"));
+
+        row_count[row-1] += 1;
+
+        
+
+    }
+
+    for i in 1..=rows {
+        row_ptr[i] = row_count[i-1] + row_ptr[i-1];
+    }
+
+    println!("{:?}", row_ptr);
+
+    println!("{:?}", values);
+
+    (values, col_indices, row_ptr, sizes[0], sizes[1], sizes[2])
+
+}
+
+
+
